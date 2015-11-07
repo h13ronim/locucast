6a49a57cd5a33bbf9cf7d47fd7a4d991b9871efb
Marcin Nowicki <pr0d1r2@gmail.com>
2015-11-07 01:39:38 +0100

    Adjust naming of feature spec

--- spec/features/welcome_spec.rb
+++ spec/features/welcome_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-describe 'homepage', :type => :feature do
+describe 'welcome', :type => :feature do
   it 'renders view' do
     visit '/'
     expect(page).to have_content('Hello, Locutus :)')
