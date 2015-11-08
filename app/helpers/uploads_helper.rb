bfdb9d24e173dd37e7dd718a59618c8297ea1f94
Jakub Gorzelak <jakub.gorzelak@gmail.com>
2015-11-08 20:04:02 +0100

    uploads/index: update layout

--- app/helpers/uploads_helper.rb
+++ app/helpers/uploads_helper.rb
@@ -8,4 +8,10 @@ module UploadsHelper
       )
     )
   end
+
+  def show_new_audiobook_cart?(uploads_count, uploads_slice_count, current_slice_index)
+    return false if uploads_count % 4 == 0
+
+    (4 * (current_slice_index + 1)) - (4 - uploads_slice_count) == uploads_count
+  end
 end
