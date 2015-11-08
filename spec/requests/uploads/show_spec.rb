620bb430c05d196d3a5e82106ced5259bef4f2ba
Jakub Gorzelak <jakub.gorzelak@gmail.com>
2015-11-08 07:45:43 +0100

    Improve uploads/show integration tests
    * Make them pass.
    * Get rid of stubs and mocks.

--- spec/requests/uploads/show_spec.rb
+++ spec/requests/uploads/show_spec.rb
@@ -24,10 +24,11 @@ describe '/uploads/show', :type => :request do
     context "when there is Upload with given id" do
       let(:upload_id) { upload.id }
       let(:upload) { create(:upload, user: user) }
-      let(:uploaded_files_ordered) { double('uploaded_files_ordered') }
+      let!(:uploaded_file_1) { create(:uploaded_file, upload: upload) }
+      let!(:uploaded_file_2) { create(:uploaded_file, upload: upload) }
 
       before do
-        allow_any_instance_of(Upload).to receive(:uploaded_files_ordered) { uploaded_files_ordered }
+        upload.update(uploaded_files_order: [uploaded_file_2.id, uploaded_file_1.id])
       end
 
       it "returns 200 response code" do
@@ -45,11 +46,10 @@ describe '/uploads/show', :type => :request do
       it 'assigns @uploaded_files' do
         subject
 
-        expect(assigns[:uploaded_files]).to eq(uploaded_files_ordered)
+        expect(assigns[:uploaded_files]).to eq([uploaded_file_2, uploaded_file_1])
       end
 
       context "and there is show_success_upload_flash param set to true passed" do
-        let(:uploaded_file) { create(:uploaded_file, user: user) }
         let(:request_url) { "/uploads/#{upload_id}?show_success_upload_flash=true" }
 
         it "returns flash message" do
