fb75317c6eb145fb6409f9c478d02ca893ff9e1f
Marcin Michalowski <marcin@stick.pl>
2015-11-08 09:38:22 +0100

    Basic welcome page layout

--- db/schema.rb
+++ db/schema.rb
@@ -36,8 +36,8 @@ ActiveRecord::Schema.define(version: 20151107181121) do
     t.string   "description"
     t.datetime "created_at",                        null: false
     t.datetime "updated_at",                        null: false
-    t.string   "author"
     t.integer  "uploaded_files_order", default: [],              array: true
+    t.string   "author"
   end
 
   add_index "uploads", ["user_id"], name: "index_uploads_on_user_id", using: :btree
