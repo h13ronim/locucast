b8d6edfa0938e80d57869eba4ee339909f1e3703
Jakub Gorzelak <jakub.gorzelak@gmail.com>
2015-11-08 12:14:55 +0100

    Redirect to uploads after sign in

---
+++ spec/requests/users/sign_in_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+
+describe '/users/sign_in', :type => :request do
+  subject { post request_url, query }
+
+  let(:request_url) { "/users/sign_in/" }
+  let(:query) do
+    {
+      "user" => {
+        "email"=>user.email,
+        "password"=>user.password,
+      }
+    }
+  end
+  let(:user) { create(:user) }
+
+  context "when credentials are valid" do
+    it "redirects to uploads/index action" do
+      subject
+
+      expect(response).to redirect_to(uploads_path)
+    end
+  end
+
+  context "when there is stored location for given user" do
+    #TODO: write test for ths
+    #it "redirects to stored location" do
+    #end
+  end
+end
