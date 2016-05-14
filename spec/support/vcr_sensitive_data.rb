2296e8cae52fc88d6335125933994a31cc478027
Jakub Gorzelak <jakub.gorzelak@gmail.com>
2016-05-14 08:35:01 +0200

    Make Rollbar settings to configurable via env variables

--- spec/support/vcr_sensitive_data.rb
+++ spec/support/vcr_sensitive_data.rb
@@ -8,6 +8,8 @@ class VcrSensitiveData < Hash
       AWS_S3_BUCKET
       AWS_S3_REGION
       TOKEN_SALT
+      ROLLBAR_ACCESS_TOKEN
+      ROLLBAR_JS_ACCESS_TOKEN
     ).each do |sensitive_variable|
       self.merge!(sensitive_variable => ENV[sensitive_variable])
     end
